4cd6b7fcd3afb4e5907636f4f00c331256e4008f
LucasBbe <lucas.boube@gmail.com>
2024-12-17 17:31:24 +0100

    finished js monorepo 5

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -19,3 +19,52 @@ insert into item(id, title, user_id)
 values
   (1, "Stuff", 1),
   (2, "Doodads", 1);
+
+create table category (
+  id INT unsigned PRIMARY KEY auto_increment NOT NULL,
+  name VARCHAR(80) NOT NULL
+);
+create table program (
+  id INT unsigned PRIMARY KEY auto_increment NOT NULL,
+  title VARCHAR(80) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  year INT NOT NULL,
+  category_id INT
+);
+
+INSERT INTO category(id, name)
+VALUES
+  (1, "Comédie"),
+  (2, "Science-Fiction");
+
+INSERT INTO program
+  (
+    id,
+    title,
+    synopsis,
+    poster,
+    country,
+    year,
+    category_id
+  )
+VALUES
+  (
+    1,
+    "The Good Place",
+    "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
+    "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
+    "USA",
+    2016,
+    1
+  ),
+  (
+    2,
+    "Dark",
+    "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
+    "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
+    "Allemagne",
+    2017,
+    2
+  );
